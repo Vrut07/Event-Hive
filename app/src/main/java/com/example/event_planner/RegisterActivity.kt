14bf5b20250b11e487520d00fc138967023d1965
Vrut07 <vruttalsaniya@gmail.com>
2025-03-06 06:55:28 -0800

    first commit

---
+++ app/src/main/java/com/example/event_planner/RegisterActivity.kt
@@ -0,0 +1,42 @@
+package com.example.event_planner
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.event_planner.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var registerBinding : ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(registerBinding.root)
+
+        registerBinding.checkTermsCondition.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked)
+            {
+                registerBinding.btnSignup.visibility = View.VISIBLE
+            }else{
+                registerBinding.btnSignup.visibility = View.GONE
+            }
+        }
+
+        registerBinding.rgGender.setOnCheckedChangeListener { group, checkedId ->
+            if(checkedId == R.id.rbMale)
+            {
+                Toast.makeText(this,"Male",Toast.LENGTH_LONG).show()
+            }
+            else if(checkedId == R.id.rbFemale)
+            {
+                Toast.makeText(this,"Female",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
