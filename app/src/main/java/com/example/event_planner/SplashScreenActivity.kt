1751bdb186cc3d93199b323ce3d8cc0449625bda
DISHA-13\91990 <dishavala7979@gmail.com>
2025-03-07 00:25:18 +0530

    Disha Vala

--- app/src/main/java/com/example/event_planner/SplashScreenActivity.kt
+++ app/src/main/java/com/example/event_planner/SplashScreenActivity.kt
@@ -3,19 +3,20 @@ package com.example.event_planner
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed(kotlinx.coroutines.Runnable {
-            startActivity(Intent(this,MainActivity::class.java))
-            finish() },3000)
+
+        // Delay splash screen for 1 second, then navigate to MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000) // 1000ms = 1 second
     }
 }
-
